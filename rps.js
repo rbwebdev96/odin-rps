1d7a34692f5f1c8ee3ee9433763d6d7834fe7c5c
Ram <146576136+rbwebdev96@users.noreply.github.com>
2025-07-25 09:30:31 +0530

    add logics to get user and computer inputs

--- rps.js
+++ rps.js
@@ -1,1 +1,26 @@
 
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3) + 1;
+  
+  if(computerChoice === 1){
+    return 'rock';
+  }else if (computerChoice === 2){
+      return 'paper';
+    }else {
+      return 'scissors';
+    }
+  }
+
+
+//console.log(getComputerChoice());
+
+function getHumanChoice() {
+  let userInput = prompt('Enter your choice: Rock, Paper or Scissors.').toLowerCase();
+  
+ // console.log(userInput);
+
+ if((userInput === 'rock')|| (userInput ==='paper') || (userInput === 'scissors'))
+  return userInput;
+}
+
+console.log(getHumanChoice());
